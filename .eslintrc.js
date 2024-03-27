9683887f309950a0f8e6b0179f2efbd74309c8c6
machenbo <1330295681@qq.com>
2024-03-27 17:57:09 +0800

    feat: 模板

---
+++ .eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/essential",
+    "plugin:vue/recommended",
+    "eslint:recommended",
+  ],
+  parserOptions: {
+    'parser': '@babel/eslint-parser',
+    "requireConfigFile":false
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    "vue/html-self-closing": ["error",{
+      "html": {
+        "void": "never",
+        "normal": "any",
+        "component": "any"
+      },
+      "svg": "always",
+      "math": "always"
+    }],
+    "vue/order-in-components": [
+      "error",
+      {
+        order: [
+          "el",
+          "key",
+          "parent",
+          "functional",
+          ["delimiters", "comments"],
+          ["components", "directives", "filters"],
+          "extends",
+          "mixins",
+          ["provide", "inject"],
+          "ROUTER_GUARDS",
+          "layout",
+          "middleware",
+          "validate",
+          "scrollToTop",
+          "transition",
+          "loading",
+          "inheritAttrs",
+          "model",
+          ["props", "propsData"],
+          "emits",
+          "setup",
+          "asyncData",
+          "data",
+          "fetch",
+          "head",
+          "computed",
+          "watch",
+          "watchQuery",
+          "LIFECYCLE_HOOKS",
+          "methods",
+          ["template", "render"],
+          "renderError",
+        ],
+      },
+    ],
+  },
+};
---
+++ src/views/home/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="home">
+    <div @click="clickfun">clickfun</div>
+    <BaseTest></BaseTest>
+    <el-button type="primary">主要按钮</el-button>
+    <i class="el-icon-delete"></i>
+    <div class="font-bold">1111</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeVue",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {
+    clickfun() {
+      this.$router.push({
+        path: "/hello",
+      });
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.wudi {
+  height: 100px;
+  border: 1px solid red;
+}
+</style>
